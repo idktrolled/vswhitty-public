558424ea573ff3c17dda3aa916d7a0ca343013bd
idktrolled <camilocarlos1100@gmail.com>
2026-03-21 22:44:57 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,14 +1,14 @@
 import Paths;
 #if !macro
  #if android
- import android.content.Context as AndroidContext;
- import android.widget.Toast as AndroidToast;
- import android.os.Environment as AndroidEnvironment;
- import android.Permissions as AndroidPermissions;
- import android.Settings as AndroidSettings;
- import android.Tools as AndroidTools;
- import android.os.Build.VERSION as AndroidVersion;
- import android.os.Build.VERSION_CODES as AndroidVersionCode;
- import android.os.BatteryManager as AndroidBatteryManager;
+ import android.content.Context;
+ import android.widget.Toast;
+ import android.os.Environment;
+ import android.Permissions;
+ import android.Settings;
+ import android.Tools;
+ import android.os.Build.VERSION;
+ import android.os.Build.VERSION_CODES;
+ import android.os.BatteryManager;
  #end
  #end
